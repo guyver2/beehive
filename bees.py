191d00db512cc32b8c3538b9427d920f03097eee
Antoine Letouzey <antoine.letouzey@gmail.com>
2016-09-08 08:46:28 +0200

    mostly debug stuff

--- bees.py
+++ bees.py
@@ -1,5 +1,5 @@
 """
-Counts Bees in and bees out of a beehive. 
+Counts Bees in and bees out of a beehive.
 Take as input a top-down view of the behive entrance and assume a fairly static
 background.
 
@@ -28,7 +28,7 @@ from munkres import linear_assignment # local import
 
 
 
-#------------   
+#------------
 
 # statistical background model, build a model from previous frames
 class BGmodel(object):
@@ -48,15 +48,15 @@ class BGmodel(object):
         self.cpt %= self.size
         if updateModel:
             self.updateModel()
-    
+
     # update the model from the current frame history
     def updateModel(self):
         # np.mean is faster but median yields better results
         self.model = np.median(self.hist, axis=0).astype(np.int32)
-    
+
     def getModel(self):
         return np.copy(self.model)
-       
+
     # substract the background to the current frame
     def apply(self, frame):
         self.add(frame)
@@ -80,21 +80,21 @@ class Bee(object):
         Bee.staticID += 1
         self.age = 0
         self.color =  tuple(255*(0.2+4*np.random.random((3))/5))
-    
+
     # move the bee to it's new position
     def move(self, pos):
         self.age += 1
         self.pos.append(list(pos))
-    
+
     # remove last position
     def pop(self):
         self.age -= 1
         self.pos.pop()
-    
+
     # compute distance to a point from given previous position
     def dist(self, pt, offset = 0):
         return math.sqrt((pt[0]-self.pos[-(1+offset)][0])**2 + (pt[1]-self.pos[-(1+offset)][1])**2)
-    
+
     # draw the bee's path as a line onto an image
     def draw(self, img):
         #print "drawing bee#%d"%self.ID
@@ -113,23 +113,23 @@ class Hive(object):
         self.y = y # Y coordinate of top left corner of the entrance
         self.w = w # width of the entrance
         self.h = h # height of the entrance
-    
+
     # new bee, check if it came from the entrance
     def append(self, pt):
         # if a bee pops out from the hive entrance, then count it
         if pt[0] > self.x and pt[0] < self.x+self.w and pt[1] > self.y and pt[1] < self.y+self.h:
             self.OUT += 1
-    
+
     # lost bee, check if it went into the entrance
     def remove(self, pt):
         # if a bee desapears closer to the hive entrance, then count it
         if pt[0] > self.x and pt[0] < self.x+self.w and pt[1] > self.y and pt[1] < self.y+self.h:
             self.IN += 1
-    
+
     # draw counters on the image
     def draw(self, img):
         cv2.putText(img,"IN  : %d OUT : %d"%(self.IN, self.OUT), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255),2)
-        
+
 
 
 
@@ -144,9 +144,12 @@ class Hive(object):
 #------------   DATASET dependent
 # input video file
 cap = cv2.VideoCapture('bees1.h264')
-# position of the hive entrance rectangle with top left corner (X,Y) and 
+if not cap.isOpened() :
+	print "error, could not find video file, please check filename"
+# position of the hive entrance rectangle with top left corner (X,Y) and
 # size (width, height)
 hive = Hive(53,412,582,62)
+DEBUG = True
 
 
 #------------   GENERIC parameters
@@ -162,35 +165,35 @@ THRESHBEE = 60
 
 # Setup SimpleBlobDetector parameters.
 params = cv2.SimpleBlobDetector_Params()
- 
+
 # Change thresholds
 params.filterByColor = False;
 #params.minThreshold = 10;
 #params.maxThreshold = 255;
- 
+
 # Filter by Area.
 params.filterByArea = True
 params.minArea = 10
 params.maxArea = 400
- 
+
 # Filter by Circularity
 params.filterByCircularity = False
 params.minCircularity = 0.70
- 
+
 # Filter by Convexity
 params.filterByConvexity = True
 params.minConvexity = 0.70
- 
+
 # Filter by Inertia
 params.filterByInertia = True
 params.minInertiaRatio = 0.1
 params.maxInertiaRatio = 0.5
- 
+
 # Create a detector with the parameters
 ver = (cv2.__version__).split('.')
 if int(ver[0]) < 3 :
     detector = cv2.SimpleBlobDetector(params)
-else : 
+else :
     detector = cv2.SimpleBlobDetector_create(params)
 
 
@@ -214,19 +217,19 @@ try :
         # convert to grayscale and apply slight blur
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         gray = cv2.GaussianBlur(gray,(5,5),0).astype(np.int32)
-        
-        
+
+
         # update the model and apply it to the current frame
         fgmask = bgm.apply(gray)
-        
+
         # Mathematical morphology to enhance the mask and remove outliers
         #fgmask = cv2.erode(fgmask,kernel,iterations = 1)
         fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
         fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_CLOSE, kernel)
-        
-        
+
+
         im_with_keypoints = None
-        
+
         # blob detection only if we gathered enough images for the background model
         if bgm.ready :
             keypoints = detector.detect(fgmask)
@@ -240,7 +243,7 @@ try :
                 # MUNKRES assignment, slightly better
                 freeBees = [True for i in xrange(len(bees))]
                 freeKP = [True for i in xrange(len(keypoints))]
-                # build cost matrix 
+                # build cost matrix
                 cost = np.zeros((len(keypoints), len(bees)))
                 for i,kp in enumerate(keypoints):
                     for j,b in enumerate(bees):
@@ -258,7 +261,7 @@ try :
                 for i in xrange(len(freeKP)): # new keypoints
                     if freeKP[i]:
                         bees.append(Bee(keypoints[i].pt))
-                        hive.append(kp.pt)                    
+                        hive.append(kp.pt)
             # remove lost bees (not seen for at least 15 frames)
             tmp = []
             for b in bees:
@@ -267,28 +270,27 @@ try :
                     b.draw(im_with_keypoints)
                     b.draw(frame)
                 else :
-                    hive.remove(b.pos[-1])            
+                    hive.remove(b.pos[-1])
             bees = tmp
             hive.draw(im_with_keypoints)
             hive.draw(frame)
             print "frame : %d /  IN : %d /  OUT : %d"%(frameid, hive.IN, hive.OUT)
             cv2.imwrite("out/frame_%05d.jpg"%frameid, frame)
-                    
-                
 
-       
+
+
+
         #debug output, uncomment to see each step
-        #cv2.imshow('frame',np.concatenate((fgmask, gray2), axis=1))
-        #cv2.imshow('model',bgm.model.astype(np.uint8))
-        #cv2.imshow('frame',gray.astype(np.uint8))
-        #cv2.imshow('mask',fgmask)
-        #if im_with_keypoints is not None : 
-        #    cv2.imshow("Keypoints", im_with_keypoints)
-        #cv2.imshow('color',frame)
-
-        # needs to be uncommented for the images to show properly
-        #if cv2.waitKey(1) & 0xFF == ord('q'):
-        #    break
+        if DEBUG :
+            cv2.imshow('background model',bgm.model.astype(np.uint8))
+            cv2.imshow('mask',fgmask)
+            if im_with_keypoints is not None :
+                cv2.rectangle(im_with_keypoints, (hive.x, hive.y), (hive.x+hive.w, hive.y+hive.h), (255,0,0), 2)
+                cv2.imshow("Keypoints", im_with_keypoints)
+            cv2.imshow('input frame',frame)
+            # wait for the key 'q' and quit
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
 except :
     # always end with an error, but sequences are too long for me to bother debuging :(
     pass
@@ -298,4 +300,3 @@ finally :
 
 print "IN : ", hive.IN
 print "OUT: ", hive.OUT
-
